5a176b3172df0018b9b1b679e63ebd6f4c61cee2
Zean <giancarlo258@hotmail.com>
2013-06-03 00:51:56 -0500

    Index, Header y Footer Presenters; configuracion de propiedasdes del proyecto y xml del appengine.

---
+++ src/com/deevo/java/client/IndexPresenter.java
@@ -0,0 +1,36 @@
+package com.deevo.java.client;
+
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.ProxyCodeSplit;
+import com.gwtplatform.mvp.client.proxy.Proxy;
+import com.google.inject.Inject;
+import com.google.gwt.event.shared.EventBus;
+import com.gwtplatform.mvp.client.proxy.RevealRootContentEvent;
+
+public class IndexPresenter extends
+		Presenter<IndexPresenter.MyView, IndexPresenter.MyProxy> {
+
+	public interface MyView extends View {
+	}
+
+	@ProxyCodeSplit
+	public interface MyProxy extends Proxy<IndexPresenter> {
+	}
+
+	@Inject
+	public IndexPresenter(final EventBus eventBus, final MyView view,
+			final MyProxy proxy) {
+		super(eventBus, view, proxy);
+	}
+
+	@Override
+	protected void revealInParent() {
+		RevealRootContentEvent.fire(this, this);
+	}
+
+	@Override
+	protected void onBind() {
+		super.onBind();
+	}
+}
--- src/com/deevo/java/client/gin/ClientModule.java
+++ src/com/deevo/java/client/gin/ClientModule.java
@@ -3,11 +3,26 @@ package com.deevo.java.client.gin;
 import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
 import com.gwtplatform.mvp.client.gin.DefaultModule;
 import com.deevo.java.client.place.ClientPlaceManager;
+import com.deevo.java.client.IndexPresenter;
+import com.deevo.java.client.IndexView;
+import com.deevo.java.client.HeaderPresenter;
+import com.deevo.java.client.HeaderView;
+import com.deevo.java.client.FooterPresenter;
+import com.deevo.java.client.FooterView;
 
 public class ClientModule extends AbstractPresenterModule {
 
 	@Override
 	protected void configure() {
 		install(new DefaultModule(ClientPlaceManager.class));
+
+		bindPresenter(IndexPresenter.class, IndexPresenter.MyView.class,
+				IndexView.class, IndexPresenter.MyProxy.class);
+
+		bindPresenter(HeaderPresenter.class, HeaderPresenter.MyView.class,
+				HeaderView.class, HeaderPresenter.MyProxy.class);
+
+		bindPresenter(FooterPresenter.class, FooterPresenter.MyView.class,
+				FooterView.class, FooterPresenter.MyProxy.class);
 	}
 }
