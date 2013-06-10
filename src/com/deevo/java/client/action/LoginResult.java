fbacfcf089477008c6c60b5a9953f6884c577401
Zean <giancarlo258@hotmail.com>
2013-06-10 13:21:33 -0500

    Arerglando Conflicto

---
+++ src/com/deevo/java/client/action/LoginResult.java
@@ -0,0 +1,9 @@
+package com.deevo.java.client.action;
+
+import com.gwtplatform.dispatch.shared.Result;
+
+public class LoginResult implements Result {
+
+	public LoginResult() {
+	}
+}
--- src/com/deevo/java/server/guice/ServerModule.java
+++ src/com/deevo/java/server/guice/ServerModule.java
@@ -2,7 +2,9 @@ package com.deevo.java.server.guice;
 
 import com.gwtplatform.dispatch.server.guice.HandlerModule;
 import com.deevo.java.client.action.NuevaPersona;
-import com.deevo.java.server.ActionHandler.NuevaPersonaActionHandler;
+import com.deevo.java.client.action.Login;
+import com.deevo.java.server.actionhandler.LoginActionHandler;
+import com.deevo.java.server.actionhandler.NuevaPersonaActionHandler;
 
 public class ServerModule extends HandlerModule {
 
@@ -10,5 +12,7 @@ public class ServerModule extends HandlerModule {
 	protected void configureHandlers() {
 
 		bindHandler(NuevaPersona.class, NuevaPersonaActionHandler.class);
+
+		bindHandler(Login.class, LoginActionHandler.class);
 	}
 }
